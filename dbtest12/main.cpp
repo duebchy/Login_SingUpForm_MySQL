f5b9644cd3f07149e73812f4537838298e608dc6
duebchy <vdovinkirill609@gmail.com>
2024-05-21 15:33:55 +0600

    added functions: delete account, logout. project finished

--- dbtest12/main.cpp
+++ dbtest12/main.cpp
@@ -48,9 +48,31 @@ void help() {
 	std::cout << "/exit - exit application" << std::endl;
 	std::cout << "/show - show data of logged user" << std::endl;
 	std::cout << "/help - show list of commands" << std::endl;
+	std::cout << "/delete - delete logged user from database" << std::endl;
+	std::cout << "/logout - logout from logged account" << std::endl;
+}
+//DELETE FROM users WHERE id = 8;
+void logout() {
+	user.login("", "", "", -1, -1);
 }
 
-
+std::string deleteUser() {
+	int temp = -1;
+	std::cout << "You are sure to delete user: " << user.getName() << ". Enter 1 to delete or 0 to stop: ";
+	std::cin >> temp;
+	if (temp == 0)  return "";
+	else if (temp == 1) {
+		query = "DELETE FROM users WHERE id = " + std::to_string(user.getId()) + "";
+		ConnCheck();
+		if (!qstate) {
+			user.login("", "", "", -1, -1);
+			return "";
+		}
+		else {
+			return "error";
+		}
+	}
+}
 std::string changeName(std::string s) {
 	query = "UPDATE users SET username = '" + s + "' WHERE username = '" + user.getName() + "'";
 	ConnCheck();
@@ -199,7 +221,7 @@ std::string parse(std::string& command) {//['/', 'l', 'o', 'g', 'i', 'n', ' ', ]
 	std::string val1 = "";
 	std::string val2 = "";							//  0    1    2    3    4    5    6   7
 	
-	if (command[0] == '/' && command[1] == 'l' && command[2] == 'o' && command[3] == 'g') {
+	if (command[0] == '/' && command[1] == 'l' && command[2] == 'o' && command[3] == 'g' && command [4] == 'i' && command[5] == 'n') {
 
 
 		int cnt = 7;
@@ -223,10 +245,10 @@ std::string parse(std::string& command) {//['/', 'l', 'o', 'g', 'i', 'n', ' ', ]
 	}
 	else if (command[0] == '/' && command[1] == 'c' && command[2] == 'p') {
 		int cnt = 4;
-			while (cnt < command.length()) {
-				
-				val1 += command[cnt];
-				cnt++;
+		while (cnt < command.length()) {
+
+			val1 += command[cnt];
+			cnt++;
 		}
 		return changePassword(val1);
 	}
@@ -240,6 +262,12 @@ std::string parse(std::string& command) {//['/', 'l', 'o', 'g', 'i', 'n', ' ', ]
 			cnt++;
 		}
 		changeName(val1);
+	}
+	else if (command[0] == '/' && command[1] == 'd' && command[2] == 'e' && command[3] == 'l') {
+		deleteUser();
+	}
+	else if (command[0] == '/' && command[1] == 'l' && command[2] == 'o' && command[3] == 'g' && command[4] == 'o' && command[5] == 'u' && command[6] == 't') {
+		logout();
 	}
 	else if(command != "") {
 		return "unknown command";
